dd8aee24222c4ae45bb0d5da02f590ad9626d8d2
99003735 <78857812+99003735@users.noreply.github.com>
2021-02-23 23:01:40 +0530

    Update mystrcpy.c

--- src/mystrcpy.c
+++ src/mystrcpy.c
@@ -1,12 +1,14 @@
-#include "mystring.h"
-char*mystrcpy(char *s1,char*s2)
-{
-    int i;
-    for(i=0;s1[i] !='\0';++i)
-    {
-        s2[i]=s1[i];
-    }
-    s2[i]='\0';
-    return s2;
 
+#include "mystring.h"
+
+char* mystrcpy(char *str1, char *str2)
+{
+    int j;
+    for(j=0;str1[j]!='\0';++j)
+    {
+        str2[j]=str1[j];
+    }
+    str[j]='\0';
+    return str2;
 }
+
