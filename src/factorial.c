4bc6226260c7d8d28fd5fbd6eee5b6b68e4f6870
99003735 <78857812+99003735@users.noreply.github.com>
2021-02-23 17:48:11 +0530

    Add files via upload

---
+++ src/factorial.c
@@ -0,0 +1,9 @@
+int fact (int n)
+{
+    int i,p=1;
+    for(i=1;i<=n;i++)
+    {
+        p=p*i;
+    }
+    return f;
+}
---
+++ src/flip.c
@@ -0,0 +1,7 @@
+int flip_number(int input,int output)
+{
+    scanf("%d",&input);
+    output= ~input;
+    return output;
+
+}
