e56c0556ad769b701177036c1db0494c0f82925c
99003735 <78857812+99003735@users.noreply.github.com>
2021-02-23 18:39:18 +0530

    Delete src directory

--- src/factorial.c
+++
@@ -1,9 +0,0 @@
-int fact (int n)
-{
-    int i,p=1;
-    for(i=1;i<=n;i++)
-    {
-        p=p*i;
-    }
-    return f;
-}
--- src/flip.c
+++
@@ -1,7 +0,0 @@
-int flip_number(int input,int output)
-{
-    scanf("%d",&input);
-    output= ~input;
-    return output;
-
-}
