75c412abba9121fc9706abd581bf244a484823f2
99003735 <78857812+99003735@users.noreply.github.com>
2021-02-23 23:20:14 +0530

    Update vsum.c

--- src/vsum.c
+++ src/vsum.c
@@ -1,1 +1,16 @@
+#include "myutils.h"
+#include "stdarg.h"
+
+double vsum(int num...)
+{
+    va_list valist;
+    double sum = 0;
+    va_start(valist,num);
+    for(int i=0;i<num;i++)
+    {
+        sum+=va_arg(valist,int);
+    }
+    va_end(valist);
+    return sum;
+}
 
