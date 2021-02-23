e7ef821372457589bfeca10ffd44435cb46349d7
99003735 <78857812+99003735@users.noreply.github.com>
2021-02-23 22:58:12 +0530

    Update mystring.h

--- inc/mystring.h
+++ inc/mystring.h
@@ -1,6 +1,9 @@
-#include<stdio.h>
-#include<string.h>
-char* mystrcat(char* str1, char* str2);
-int mystrlen(char *s1);
-int mystrcmp(char str1[],char str2[]);
-char*mystrcpy(char *s1,char*s2);
+#ifndef  MYSTRING.h
+#define MYSTRING.h
+
+char* mystrcat(char *str1, char *str2);
+int str_Compare(char str1[], char str2[]);
+char* mystrcpy(char *str1, char *str2);
+int mystrlen(chat *str);
+
+#endif
